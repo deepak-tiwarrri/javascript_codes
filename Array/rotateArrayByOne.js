df647347beab220d39beb2bc3d059c5467de5676
DeepakPC <ainutiwari483@gmail.com>
2025-06-09 23:32:18 +0530

    added rotate array by one

---
+++ Array/rotateArrayByOne.js
@@ -0,0 +1,15 @@
+function rotateArrayByOne(nums){
+   let temp = nums[0];
+   for(let i=1;i<nums.length;i++){
+      nums[i-1] = nums[i];
+   }
+   nums[nums.length-1] = temp;
+   return nums;
+}
+
+// let nums =  [1, 2, 3, 4, 5];
+let nums =  [-1, 0, 3, 6];
+let result = rotateArrayByOne(nums);
+for(let val of result){
+   console.log(val," ");
+}
