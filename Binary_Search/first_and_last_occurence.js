a8a65d065cd84aa7a5abf5f681592233b2eae307
Deepak Tiwari <tiwarideepak8707@gmail.com>
2026-06-28 16:31:09 +0530

    feat: refactor first and last occurrence functions into a Solution class; implement binary search for improved performance

--- Binary_Search/first_and_last_occurence.js
+++ Binary_Search/first_and_last_occurence.js
@@ -1,34 +1,52 @@
-/**
- * Finds the first and last index of the target{x} element
- * @param {Array} nums
- * @param {number} x
- * @returns {Object} {firstIndex,lastIndex}
- */
-function firstAndLastOccurence(nums, x) {
-  //brute force appraoch
-  let first = -1,
-    last = -1;
-  //brute force appraoch takes O(n)
-  // for (let i = 0; i < nums.length; i++) {
-  //   if (nums[i] === x) {
-  //     if (first === -1) {
-  //       first = i;
-  //     }
-  //     last = i;
-  //   }
-  // }
-
-  first = findFirstOccurence(nums, x);
-  last = findLastOccurence(nums, x);
-  return { first, last };
+class Solution {
+  findFirstOccurence(nums, x) {
+    let ans = -1;
+    let low = 0, high = nums.length;
+    while (low <= high) {
+      let mid = low + Math.floor((high - low) / 2);
+      if (nums[mid] === x) {
+        ans = mid;
+        high = mid - 1;
+      } else if (nums[mid] > x) {
+        high = mid - 1;
+      } else {
+        low = mid + 1;
+      }
+    }
+    return ans;
+  }
+  findLastOccurence(nums, x) {
+    let ans = -1;
+    let low = 0, high = nums.length;
+    while (low <= high) {
+      let mid = low + Math.floor((high - low) / 2);
+      if (nums[mid] === x) {
+        ans = mid;
+        low = mid+1;
+      } else if (nums[mid] > x) {
+        high = mid - 1;
+      } else {
+        low = mid + 1;
+      }
+    }
+    return ans;
+  }
+  /**
+   * Finds the first and last index of the target{x} element
+   * @param {Array} nums
+   * @param {number} x
+   * @returns {Object} {firstIndex,lastIndex}
+   */
+  searchRange(nums, target) {
+    let first = this.findFirstOccurence(nums,target);
+    let last = this.findLastOccurence(nums,target);
+    return [first,last];
+  }
 }
 
-function findFirstOccurence(nums, x) {
-  
-}
-function findLastOccurence(nums,x) {
-  
-}
+
 const nums = [2, 4, 6, 8, 8, 8, 8, 11, 13];
-const result = firstAndLastOccurence(nums, 8);
-console.log(result);
+const target = 8;
+const sol = new Solution();
+let result = sol.searchRange(nums, target);
+console.log(`first and last occurence of ${target}: ${result}`);
--- Binary_Search/search_in_rotated_sorted_array.js
+++ Binary_Search/search_in_rotated_sorted_array.js
@@ -24,8 +24,40 @@ function searchInRotatedSortedArray(nums, target) {
   }
   return -1;
 }
+class Solution {
+  search(nums, k) {
+    let low = 0, high = nums.length - 1;
+    let ans = -1;
+    while (low <= high) {
+      let mid = low + Math.floor((high - low) / 2);
+      if (nums[mid] === k) return mid;
+      else if (nums[low] <= nums[mid]) {
+        //that means left half is sorted
+        //check if the value exist in this half or not
+        if (nums[low] <= k && k <= nums[mid])
+          //it means it is in range so move the high to this side
+          high = mid - 1;
+        else
+          low = mid + 1;
+      } else {
+        //that means right half is sorted
+        if(nums[mid]<=k && k<=nums[high]) low = mid+1;
+        else high = mid-1;
+      }
+    }
+    return ans;
+  }
+}
+
+class Solution {
+    searchInARotatedSortedArrayII(nums, k) {
+       
+    }
+}
 
 const nums = [8, 9, 10, 11, 1, 2, 3, 4, 5, 6, 7];
 const target = 4;
-const result = searchInRotatedSortedArray(nums, target);
+// const result = searchInRotatedSortedArray(nums, target);
+const sol = new Solution();
+const result = sol.search(nums,target);
 console.log(result);
